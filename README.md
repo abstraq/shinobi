c24982b030c250fbb9286a73887c2afc236b3547
abstraq <abstraq@outlook.com>
2021-06-21 14:58:59 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# shinobi
+Shinobi is a moderation bot for discord that aims to provide useful moderation commands and zero bloat.
+
+
+### Building
+To build Shinobi you need:
+* Java 16 JDK or newer
+* Git
+
+Open your terminal and run the following commands:
+```sh
+git clone https://github.com/abstraq/shinobi.git
+cd shinobi/
+./gradlew build
+```
+The output will be in the `build/libs` directory.
+
+### License
+
+Shinobi is licensed under the [GPLv3](https://github.com/abstraq/shinobi/blob/master/LICENSE) © [Abstraq Software](https://github.com/abstraq).
---
+++ src/main/java/me/abstraq/shinobi/Shinobi.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2021 Abstraq Software
+ *
+ * Shinobi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shinobi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shinobi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.abstraq.shinobi;
+
+import javax.security.auth.login.LoginException;
+import net.dv8tion.jda.api.GatewayEncoding;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main class for Shinobi.
+ */
+public final class Shinobi {
+    private final Logger logger;
+    private final JDA api;
+
+    private Shinobi() {
+        this.logger = LoggerFactory.getLogger(Shinobi.class);
+        try {
+            this.api = JDABuilder.createLight(System.getenv("DISCORD_TOKEN"))
+                .setActivity(Activity.watching("over this server."))
+                .setGatewayEncoding(GatewayEncoding.ETF)
+                .build();
+        } catch (LoginException e) {
+            this.logger.error("Error while starting shinobi: {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Entry point for shinobi.
+     *
+     * @param args arguments passed to the program by the user.
+     */
+    public static void main(String[] args) {
+        new Shinobi();
+    }
+}
